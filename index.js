e1d38f2e7ec60cedbfd8f494b6e2fc066c14cf0a
Ossi <ossijuhani.ikonen@gmail.com>
2025-01-03 17:35:26 +0200

    Updated index.js

--- index.js
+++ index.js
@@ -60,13 +60,12 @@ app.use(session({
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
-        secure: true, // Set `secure: true` in production with HTTPS
+        secure: false, // Set `secure: true` in production with HTTPS
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7, // Cookie expires in 1 week
         maxAge: Date.now() + 1000 * 60 * 60 * 24 * 7, // Cookie's max age is 1 week
     }
 }));
 app.use(flash());
-app.set("trust proxy", 1); // trust first proxy 
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
